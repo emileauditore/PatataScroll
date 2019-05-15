3a16ad998fb63093b0d5635db02375b4b060f705
Emilio <emilio@mail.com>
2019-05-15 17:51:30 +0200

    Añade delegados chungos

--- PatataScroll/ViewController.swift
+++ PatataScroll/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 //hola
     //que tal
+    //movidas
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
